a186f8809ee7c8615d5e6b59f32b0c3562aac034
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-10-08 16:44:51 +0300

    fix writing config file promise

--- core/app-config.service.ts
+++ core/app-config.service.ts
@@ -3,29 +3,32 @@ const fs = require('fs');
 const path = require('path');
 import {FileSystemService} from './file-system.service'
 
+let configPath = path.join(__dirname, '..', 'app-config.json');
+
 ipcMain.on('readConfigFile', (event, arg) => {
-  let configPath = path.join(__dirname, '..', 'app-config.json');
   FileSystemService.checkPathExist(configPath).then(stat => {
     if (!stat) {
       return generateDefaultConfig();
     }
-  }).then(fs.readFile(path.join(__dirname, '..', 'app-config.json'), (err, data) => {
-    if (err) {
-      err.message = "Error reading config file. Config file does not exist. " + err.message;
-      throw err;
-    }
-    try {
-      let json = JSON.parse(data);
-      event.sender.send('readConfigFile-reply', json)
-    } catch(err) {
-      err.message = "Error on parsing config file. " + err.message;
-      throw err;
-    }
-  }));
+  }).then(() => {
+    fs.readFile(configPath, (err, data) => {
+      if (err) {
+        err.message = "Error reading config file. Config file does not exist. " + err.message;
+        throw err;
+      }
+      try {
+        let json = JSON.parse(data);
+        event.sender.send('readConfigFile-reply', json)
+      } catch(err) {
+        err.message = "Error on parsing config file. " + err.message;
+        throw err;
+      }
+    });
+  });
 });
 
 ipcMain.on('writeConfigFile', (event, arg) => {
-  fs.writeFile(path.join(__dirname, '..', 'app-config.json'), JSON.stringify(arg, null, 2), (err, data) => {
+  fs.writeFile(configPath, JSON.stringify(arg, null, 2), (err, data) => {
     if (err) {
       err.message = "Error writing config file. " + err.message;
       event.sender.send('writeConfigFile-reply', false);
@@ -48,7 +51,7 @@ function generateDefaultConfig() {
     }
   };
   return new Promise((resolve, reject) => {
-    fs.writeFile(path.join(__dirname, '..', 'app-config.json'), JSON.stringify(defaultConfig, null, 2), (err, data) => {
+    fs.writeFile(configPath, JSON.stringify(defaultConfig, null, 2), (err, data) => {
       if (err) {
         err.message = "Error writing config file. " + err.message;
         reject();

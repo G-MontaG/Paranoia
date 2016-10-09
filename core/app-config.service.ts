4dbfe46cd87749585a9e01b19d9e57c0c017f75d
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-10-09 16:20:06 +0300

    add readFile, writeFile promises function Rewrite AppConfigService to promises use.

--- core/app-config.service.ts
+++ core/app-config.service.ts
@@ -1,63 +1,75 @@
 const {ipcMain} = require('electron');
-const fs = require('fs');
 const path = require('path');
-import {FileSystemService} from './file-system.service'
+import {FileSystemService} from "./file-system.service";
 
-let configPath = path.join(__dirname, '..', 'app-config.json');
+class AppConfig {
+  private _configPath;
 
-ipcMain.on('readConfigFile', (event, arg) => {
-  FileSystemService.checkPathExist(configPath).then(stat => {
-    if (!stat) {
-      return generateDefaultConfig();
-    }
-  }).then(() => {
-    fs.readFile(configPath, (err, data) => {
-      if (err) {
-        err.message = "Error reading config file. Config file does not exist. " + err.message;
-        throw err;
-      }
-      try {
-        let json = JSON.parse(data);
-        event.sender.send('readConfigFile-reply', json)
-      } catch(err) {
-        err.message = "Error on parsing config file. " + err.message;
-        throw err;
-      }
+  constructor() {
+    this._configPath = path.join(__dirname, '..', 'app-config.json');
+
+    this._readConfigFile();
+    this._writeConfigFile();
+  }
+
+  private _readConfigFile() {
+    var self = this;
+    ipcMain.on('readConfigFile', (event, arg) => {
+      FileSystemService.stat(self._configPath)
+        .then(stat => {
+          if (!stat) {
+            return self._generateDefaultConfig();
+          }
+        })
+        .then(() => {
+          return FileSystemService.readFile(self._configPath);
+        })
+        .then((data: string) => {
+          try {
+            let json = JSON.parse(data);
+            event.sender.send('readConfigFile-reply', json)
+          } catch (err) {
+            err.message = "Error on parsing config file. " + err.message;
+            throw err;
+          }
+        })
+        .catch((err) => {
+          err.message = "Error reading config file. " + err.message;
+          throw err;
+        });
     });
-  });
-});
+  }
 
-ipcMain.on('writeConfigFile', (event, arg) => {
-  fs.writeFile(configPath, JSON.stringify(arg, null, 2), (err, data) => {
-    if (err) {
-      err.message = "Error writing config file. " + err.message;
-      event.sender.send('writeConfigFile-reply', false);
-      throw err;
-    }
-    event.sender.send('writeConfigFile-reply', true);
-  });
-});
-
-function generateDefaultConfig() {
-  let defaultConfig = {
-    fileManagementConfig: {
-      root: "/path"
-    },
-    keyStorageConfig: {
-      root: "/path"
-    },
-    connectionConfig: {
-      root: "/path"
-    }
-  };
-  return new Promise((resolve, reject) => {
-    fs.writeFile(configPath, JSON.stringify(defaultConfig, null, 2), (err, data) => {
-      if (err) {
-        err.message = "Error writing config file. " + err.message;
-        reject();
-        throw err;
-      }
-      resolve();
+  private _writeConfigFile() {
+    var self = this;
+    ipcMain.on('writeConfigFile', (event, arg) => {
+      FileSystemService.writeFile(self._configPath, JSON.stringify(arg, null, 2))
+        .then(() => {
+          event.sender.send('writeConfigFile-reply', true);
+        })
+        .catch((err) => {
+          err.message = "Error writing config file. " + err.message;
+          event.sender.send('writeConfigFile-reply', false);
+          throw err;
+        });
     });
-  });
+  }
+
+  private _generateDefaultConfig() {
+    let self = this;
+    let defaultConfig = {
+      fileManagementConfig: {
+        root: "/path"
+      },
+      keyStorageConfig: {
+        root: "/path"
+      },
+      connectionConfig: {
+        root: "/path"
+      }
+    };
+    return FileSystemService.writeFile(self._configPath, JSON.stringify(defaultConfig, null, 2));
+  }
 }
+
+new AppConfig();
--- core/file-system.service.ts
+++ core/file-system.service.ts
@@ -1,15 +1,49 @@
 const fs = require('fs');
+const path = require('path');
 
 export class FileSystemService {
   constructor() {
 
   }
 
-  static checkPathExist(path: string) {
+  public static stat(path: string) {
     return new Promise((resolve, reject) => {
       fs.stat(path, (err, stats) => {
-        if(err) {
+        if (err) {
           resolve(false);
+        }
+        resolve(stats);
+      });
+    });
+  }
+
+  public static writeFile(path: string | Buffer, data: string | Buffer, options?: Object | string) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(path, data, options, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
+  }
+
+  public static readFile(path: string | Buffer, options?: Object | string) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, options, (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  public static mkdir(path: string | Buffer, mode?: number) {
+    return new Promise((resolve, reject) => {
+      fs.mkdir(path, mode, (err, stats) => {
+        if (err) {
+          reject(err);
         }
         resolve(stats);
       });

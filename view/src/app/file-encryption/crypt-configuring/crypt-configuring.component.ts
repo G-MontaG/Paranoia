c60e9444e99a502293cf58cc5bad6359d46596f1
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-10-28 11:14:29 +0300

    move password generator to service

--- view/src/app/file-encryption/crypt-configuring/crypt-configuring.component.ts
+++ view/src/app/file-encryption/crypt-configuring/crypt-configuring.component.ts
@@ -3,7 +3,6 @@ import {Router} from "@angular/router";
 import {FileEncryptionService} from "../service/file-encryption.service";
 import {CryptConfiguringService} from "./service/crypt-configuring.service";
 import {FormControl, FormGroup, FormBuilder, Validators} from "@angular/forms";
-const passwordGenerator = require('password-generator');
 
 @Component({
   selector: 'crypt-configuring',
@@ -16,8 +15,6 @@ export class CryptConfiguringComponent implements OnInit, OnDestroy, AfterViewIn
   public algorithm: FormControl;
   public manualPassword: FormControl;
   public manualSalt: FormControl;
-
-  private _minLength: number = 12;
 
   constructor(private _router: Router,
               private _fb: FormBuilder,
@@ -64,47 +61,9 @@ export class CryptConfiguringComponent implements OnInit, OnDestroy, AfterViewIn
     });
   }
 
-  public setPasswordSize(size: number) {
-    this._minLength = size;
-  }
-
   public generatePassword(event) {
     event.stopPropagation();
-    let self = this;
-    let uppercaseMinCount = 3;
-    let lowercaseMinCount = 3;
-    let numberMinCount = 2;
-    let specialMinCount = 2;
-    let UPPERCASE_RE = /([A-Z])/g;
-    let LOWERCASE_RE = /([a-z])/g;
-    let NUMBER_RE = /([\d])/g;
-    let SPECIAL_CHAR_RE = /([\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}])/g;
-    let NON_REPEATING_CHAR_RE = /([\w\d\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}])\1{2,}/g;
-
-    function isStrongEnough(password) {
-      let uc = password.match(UPPERCASE_RE);
-      let lc = password.match(LOWERCASE_RE);
-      let n = password.match(NUMBER_RE);
-      let sc = password.match(SPECIAL_CHAR_RE);
-      let nr = password.match(NON_REPEATING_CHAR_RE);
-      return password.length >= self._minLength &&
-        !nr &&
-        uc && uc.length >= uppercaseMinCount &&
-        lc && lc.length >= lowercaseMinCount &&
-        n && n.length >= numberMinCount &&
-        sc && sc.length >= specialMinCount;
-    }
-
-    function createPassword() {
-      let password = "";
-      while (!isStrongEnough(password)) {
-        password = passwordGenerator(self._minLength, false,
-          /[\w\d\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}]/);
-      }
-      return password;
-    }
-
-    this.manualPassword.patchValue(createPassword());
+    this.manualPassword.patchValue(this.cryptConfiguringService.generatePassword());
   }
 
   public generateSalt(event) {
--- view/src/app/file-encryption/crypt-configuring/service/crypt-configuring.service.ts
+++ view/src/app/file-encryption/crypt-configuring/service/crypt-configuring.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+const passwordGenerator = require('password-generator');
 
 @Injectable()
 export class CryptConfiguringService {
@@ -37,6 +38,51 @@ export class CryptConfiguringService {
 
     'aes-256-xts'
   ];
+
+  private _minLength: number = 12;
+
   constructor() {
+  }
+
+  public setPasswordSize(size: number) {
+    this._minLength = size;
+  }
+
+  public generatePassword() {
+    let self = this;
+    let uppercaseMinCount = 3;
+    let lowercaseMinCount = 3;
+    let numberMinCount = 2;
+    let specialMinCount = 2;
+    let UPPERCASE_RE = /([A-Z])/g;
+    let LOWERCASE_RE = /([a-z])/g;
+    let NUMBER_RE = /([\d])/g;
+    let SPECIAL_CHAR_RE = /([\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}])/g;
+    let NON_REPEATING_CHAR_RE = /([\w\d\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}])\1{2,}/g;
+
+    function isStrongEnough(password) {
+      let uc = password.match(UPPERCASE_RE);
+      let lc = password.match(LOWERCASE_RE);
+      let n = password.match(NUMBER_RE);
+      let sc = password.match(SPECIAL_CHAR_RE);
+      let nr = password.match(NON_REPEATING_CHAR_RE);
+      return password.length >= self._minLength &&
+        !nr &&
+        uc && uc.length >= uppercaseMinCount &&
+        lc && lc.length >= lowercaseMinCount &&
+        n && n.length >= numberMinCount &&
+        sc && sc.length >= specialMinCount;
+    }
+
+    function createPassword() {
+      let password = "";
+      while (!isStrongEnough(password)) {
+        password = passwordGenerator(self._minLength, false,
+          /[\w\d\!\@\#\$\%\^\&\*\(\)\=\_\+\,\.\/\<\>\?\;\'\:\"\|\{\}]/);
+      }
+      return password;
+    }
+
+    return createPassword();
   }
 }

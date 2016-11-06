a632ec1f791c323a8919f13b2b3b94f683f5ab48
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-11-06 21:44:46 +0200

    return camellia algorithm filter algorithms only once on creation

--- core/file-encryption/algorithms-config.service.ts
+++ core/file-encryption/algorithms-config.service.ts
@@ -4,7 +4,17 @@ const crypto = require('crypto');
 
 export class AlgorithmConfigService {
 
+  private algorithmList = [];
+
   constructor() {
+    let supportedList = crypto.getCiphers();
+    _.forEach(this.config, (configItem) => {
+      if(_.findIndex(supportedList, function(item) {
+          return item === configItem.algorithm;
+        }) > -1) {
+        this.algorithmList.push(configItem);
+      }
+    });
     this.sendAlgorithmList();
   }
 
@@ -108,6 +118,27 @@ export class AlgorithmConfigService {
       ivLength: 8
     },
     {
+      algorithm: "camellia-256-ofb",
+      name: 'Camellia 256 OFB',
+      requireTag: false,
+      keyLength: 32,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-192-ofb",
+      name: 'Camellia 192 OFB',
+      requireTag: false,
+      keyLength: 24,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-128-ofb",
+      name: 'Camellia 128 OFB',
+      requireTag: false,
+      keyLength: 16,
+      ivLength: 16
+    },
+    {
       algorithm: "rc2-ofb",
       name: 'RC2 OFB',
       requireTag: false,
@@ -172,6 +203,27 @@ export class AlgorithmConfigService {
       ivLength: 8
     },
     {
+      algorithm: "camellia-256-cfb",
+      name: 'Camellia 256 CFB',
+      requireTag: false,
+      keyLength: 32,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-192-cfb",
+      name: 'Camellia 192 CFB',
+      requireTag: false,
+      keyLength: 24,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-128-cfb",
+      name: 'Camellia 128 CFB',
+      requireTag: false,
+      keyLength: 16,
+      ivLength: 16
+    },
+    {
       algorithm: "rc2-cfb",
       name: 'RC2 CFB',
       requireTag: false,
@@ -234,6 +286,27 @@ export class AlgorithmConfigService {
       requireTag: false,
       keyLength: 32,
       ivLength: 8
+    },
+    {
+      algorithm: "camellia-256-cbc",
+      name: 'Camellia 256 CBC',
+      requireTag: false,
+      keyLength: 32,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-192-cbc",
+      name: 'Camellia 192 CBC',
+      requireTag: false,
+      keyLength: 24,
+      ivLength: 16
+    },
+    {
+      algorithm: "camellia-128-cbc",
+      name: 'Camellia 128 CBC',
+      requireTag: false,
+      keyLength: 16,
+      ivLength: 16
     },
     {
       algorithm: "rc2-cbc",
@@ -338,16 +411,7 @@ export class AlgorithmConfigService {
 
   public sendAlgorithmList() {
     ipcMain.on(`cryptConfiguringGetAlgorithms`, (event, arg) => {
-      let supportedList = crypto.getCiphers();
-      let algorithmList = [];
-      _.forEach(this.config, (configItem) => {
-        if(_.findLastIndex(supportedList, function(item) {
-            return item === configItem.algorithm;
-        }) > -1) {
-          algorithmList.push(configItem);
-        }
-      });
-      event.sender.send('cryptConfiguringGetAlgorithms-reply', algorithmList);
+      event.sender.send('cryptConfiguringGetAlgorithms-reply', this.algorithmList);
     });
   }
 }
